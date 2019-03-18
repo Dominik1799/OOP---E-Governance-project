c64fef606ba8a550cf0226eadea9e27023cd67ea
Dominik Horvath <dom.horvath@gmail.com>
2019-03-18 22:21:55 +0100

    first commit

---
+++ src/sample/ParkingLot.java
@@ -0,0 +1,6 @@
+package sample;
+
+public class ParkingLot {
+    private int numberOfSpots;
+
+}
---
+++ src/sample/User.java
@@ -0,0 +1,8 @@
+package sample;
+
+public class User {
+    private String Name;
+    private String Surename;
+    private String CarType;
+
+}
