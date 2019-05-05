d3e45a1fcf8c84f1b65b66d4244bc54f5ec6a269
Dominik Horvath <dom.horvath@gmail.com>
2019-05-05 23:22:24 +0200

    test

--- src/Users/DisabledPerson.java
+++
@@ -1,7 +0,0 @@
-package Users;
-
-public class DisabledPerson extends User {
-    public DisabledPerson(String name, String surename, String typeOfCar,String carId,Double credit) {
-        super(name, surename, typeOfCar,carId,credit);
-    }
-}
--- src/Users/Student.java
+++
@@ -1,8 +0,0 @@
-package Users;
-
-public class Student extends User implements user_interface {
-    public Student(String name, String surename, String typeOfCar,String Carid,Double credit) {
-        super(name, surename, typeOfCar,Carid,credit);
-    }
-
-}
